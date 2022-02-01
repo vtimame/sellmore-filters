4e790c064ada4d6746690c8898a7ac61c624d044
Timur Valitov <vtima.me@gmail.com>
2022-02-01 11:28:46 +0300

    Initial commit

---
+++ nuxt.config.ts
@@ -0,0 +1,21 @@
+import type { NuxtConfig } from "@nuxt/types";
+
+const config: NuxtConfig = {
+  env: {},
+  head: {
+    title: "Sellmore filters",
+    meta: [
+      { charset: "utf-8" },
+      { name: "viewport", content: "width=device-width, initial-scale=1" },
+      { hid: "description", name: "description", content: "" },
+    ],
+    link: [],
+  },
+  modules: [],
+  plugins: [],
+  css: [],
+  build: {},
+  buildModules: ["@nuxt/typescript-build"],
+};
+
+export default config;
